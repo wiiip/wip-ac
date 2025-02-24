1f91eef77174b0e9d0aab12022453f7d62417502
Bridger Tower <bridgertower@gmail.com>
2025-02-24 10:29:20 -0700

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -67,16 +67,34 @@ export default function Home() {
   return (
     <main className="p-6 grid gap-12 uppercase font-light">
       <PixelRearrange />
-
       <div>
         <h1 className="font-medium">Work In Progress / Applied Creative</h1>
         <p>
           A design and development company by{" "}
-          <OutLink href="https://bridger.to">Bridger Tower</OutLink>.
+          <a
+            href="https://bridger.to"
+            className="transition-all hover:text-blue-800 text-blue-600"
+          >
+            Bridger Tower
+            <svg
+              width="12"
+              height="12"
+              viewBox="0 0 12 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="inline-flex mb-1"
+            >
+              <path
+                d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z"
+                fill="currentColor"
+                fillRule="evenodd"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </a>
         </p>
       </div>
-
-      <div>
+      <div className="space-y-2 sm:space-y-0">
         <h2 className="sr-only">Currently Building</h2>
         {Projects.map((project) => (
           <OutLink block key={project.name} href={project.href}>
@@ -84,7 +102,6 @@ export default function Home() {
           </OutLink>
         ))}
       </div>
-
       <div>
         <p>contact me at bt @ wip.ac</p>
         <p>© work in progress llc 2025</p>
@@ -105,7 +122,7 @@ const OutLink = ({
   return (
     <a
       href={href}
-      className={`w-fit transition-all hover:text-blue-800 text-blue-600 ${
+      className={`sm:w-fit transition-all hover:text-blue-800 text-blue-600 p-3 sm:p-0 border sm:border-0 ${
         block ? "block" : "inline"
       }`}
     >
