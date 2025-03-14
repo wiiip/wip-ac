77f26a17638499d571cb8db487b690fc976d7f1e
Bridger Tower <bridgertower@gmail.com>
2025-03-14 12:01:47 -0600

    fresh

--- app/page.tsx
+++ app/page.tsx
@@ -1,136 +1,103 @@
-import PixelRearrange from "@/app/wip";
-
-const Projects = [
-  {
-    name: "Vercel",
-    href: "https://vercel.com",
-  },
-  {
-    name: "Julius",
-    href: "https://julius.ai",
-  },
-  {
-    name: "Laravel",
-    href: "https://laravel.com",
-  },
-  {
-    name: "Route",
-    href: "https://route.com",
-  },
-  {
-    name: "WIP Design",
-    href: "https://wipdes.com",
-  },
-  {
-    name: "Router.so",
-    href: "https://router.so",
-  },
-  {
-    name: "Outr",
-    href: "https://outr.io",
-  },
-  {
-    name: "Wavefinder",
-    href: "https://wavefinder.io",
-  },
-  {
-    name: "SwyftFin",
-    href: "https://swyftfin.com",
-  },
-  {
-    name: "Rem Blair",
-    href: "https://remblair.com",
-  },
-  {
-    name: "Ampry",
-    href: "https://ampry.com",
-  },
-  {
-    name: "ASAP Engineering",
-    href: "https://studiomojave.com",
-  },
-  {
-    name: "Studio Mojave",
-    href: "https://studiomojave.com",
-  },
-  {
-    name: "Travel Mellow",
-    href: "https://travelmellow.com",
-  },
-  {
-    name: "Zion",
-    href: "https://zion.surf",
-  },
-];
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="p-6 grid gap-12 uppercase font-light">
-      <PixelRearrange />
-      <div>
-        <h1 className="font-normal">Work In Progress / Applied Creative</h1>
-        <h2>
-          A design and development company by{" "}
-          <a
-            href="https://bridger.to"
-            className="transition-all hover:text-blue-800 text-blue-600"
-          >
-            Bridger Tower
-            <svg
-              width="12"
-              height="12"
-              viewBox="0 0 12 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="inline-flex mb-1"
-            >
-              <path
-                d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9.00001C12 9.27615 11.7761 9.50001 11.5 9.50001C11.2239 9.50001 11 9.27615 11 9.00001V4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z"
-                fill="currentColor"
-                fillRule="evenodd"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </a>
-        </h2>
-      </div>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2 tracking-[-.01em]">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+              app/page.tsx
+            </code>
+            .
+          </li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
 
-      <div className="space-y-2 sm:space-y-0">
-        <h3 className="sr-only">PROJECTS / PARTNERS</h3>
-        {Projects.map((project) => (
-          <OutLink block key={project.name} href={project.href}>
-            {project.name}
-          </OutLink>
-        ))}
-      </div>
-      <div>
-        <h3 className="sr-only">ABOUT</h3>
-        <h3>Made at the crossroads of Design, Engnineering, and Marketing.</h3>
-        <p className="text-zinc-600">contact me at bt @ wip.ac</p>
-        <p className="text-zinc-600">© work in progress llc 2025</p>
-      </div>
-    </main>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
   );
 }
-
-const OutLink = ({
-  href,
-  children,
-  block,
-}: {
-  href: string;
-  children: React.ReactNode;
-  block?: boolean;
-}) => {
-  return (
-    <a
-      href={href}
-      className={`sm:w-fit group transition-all hover:text-blue-800 text-blue-600 p-3 sm:p-0 border sm:border-0 ${
-        block ? "block" : "inline"
-      }`}
-    >
-      <span className="hidden group-hover:inline">{"> "}</span>
-      {children}
-    </a>
-  );
-};
