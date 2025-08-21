b827f70740de59aeca8176317e6ca8f4f8430350
Bridger Tower <bridgertower@gmail.com>
2025-08-21 12:21:07 -0600

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,26 @@
 import Image from "next/image";
 import Logo from "@/public/logo.svg";
 import Icon from "@/public/icon.svg";
+import Link from "next/link";
 
 import { Shader } from "./shader";
 
 export default function Home() {
   return (
-    <main content="relative">
-      <Shader />
-      <Image src={Icon} alt="WIP ICON"></Image>
-      <Image src={Logo} alt="WIP LOGO"></Image>
+    <main className="relative h-screen w-screen overflow-hidden">
+      <section className="z-50 fixed top-6 left-6 flex gap-6">
+        <Image src={Icon} height={48} alt="WIP ICON"></Image>
+        <Image src={Logo} height={36} alt="WIP LOGO"></Image>
+      </section>
+      <section className="z-50 fixed bottom-6 right-6 text-white">
+        <Link href="/about">&rarr; about</Link>
+      </section>
+      <Shader
+        className="fixed w-full h-full top-0 right-0 -z-10"
+        colorBack="#222"
+        colorFront="#f5f5f5"
+        speed={0.2}
+      />
     </main>
   );
 }
--- app/shader.tsx
+++ app/shader.tsx
@@ -3,10 +3,5 @@
 import { Dithering, DitheringProps } from "@paper-design/shaders-react";
 
 export function Shader(props: DitheringProps) {
-  return (
-    <Dithering
-      style={{ position: "fixed", width: "100%", height: "100%" }}
-      {...props}
-    />
-  );
+  return <Dithering {...props} />;
 }
